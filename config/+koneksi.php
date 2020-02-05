8835af0e99ecaa13f4e7e46e416764fbb4e0bf77
errics7 <ebernedy@gmail.com>
2020-02-05 18:13:48 +0700

    update confg database

--- config/+koneksi.php
+++ config/+koneksi.php
@@ -1,8 +1,8 @@
 <?php 
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$database = "skripsi";
+$host = "us-cdbr-iron-east-04.cleardb.net";
+$user = "b943b7451280a8";
+$pass = "97500870";
+$database = "heroku_0e57a2415c1dbaa";
 
 ?>
