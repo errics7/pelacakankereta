aee2544a348bef66288b2cc4daedfa77077afa7b
errics7 <ebernedy@gmail.com>
2020-02-05 19:46:22 +0700

    update time +7

--- models/m_rfid_insert.php
+++ models/m_rfid_insert.php
@@ -10,7 +10,7 @@ class Rfid{
     public function uptd($UID, $Dest, $Prev, $Cur, $Next){
         $db = $this->mysqli->conn;
         $sql = "INSERT INTO `pemantauan_kereta`(`UID`, `destination`, `prev_station`, `current_station`, `waktu_tiba_curr`, `next_station`) 
-                VALUES ('$UID','$Dest','$Prev','$Cur',now(),'$Next')";
+                VALUES ('$UID','$Dest','$Prev','$Cur',now()+ INTERVAL 1 HOUR,'$Next')";
         $query = $db->query($sql) or die ($db->error);
     }
 }
